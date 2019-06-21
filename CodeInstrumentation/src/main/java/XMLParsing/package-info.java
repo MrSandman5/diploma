eed7655c22eec25c9e9360ff880ae3a2abb8a705
MrSandman5 <mrsandman138@gmail.com>
2019-06-21 14:31:36 +0300

    Delete previous

--- CodeInstrumentation/src/main/java/XMLParsing/package-info.java
+++
@@ -1,7 +0,0 @@
-@XmlSchema(
-        namespace = "Settings",
-        elementFormDefault = XmlNsForm.QUALIFIED)
-@XmlAccessorType(XmlAccessType.FIELD)
-package XMLParsing;
-
-import javax.xml.bind.annotation.*;
